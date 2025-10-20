c2f04f10def619aa959d1c8133992940d036718f
Asyad Nazeem <110421551+AsyadNazeem@users.noreply.github.com>
2025-10-20 23:46:00 +0530

    Add files via upload

---
+++ employee_salary.sql
@@ -0,0 +1,72 @@
+-- MySQL dump 10.13  Distrib 9.4.0, for macos26.0 (arm64)
+--
+-- Host: 127.0.0.1    Database: Data Analysis
+-- ------------------------------------------------------
+-- Server version	9.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee_salary`
+--
+
+DROP TABLE IF EXISTS `employee_salary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee_salary` (
+  `employee_id` int NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `occupation` varchar(50) DEFAULT NULL,
+  `salary` int DEFAULT NULL,
+  `dept_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee_salary`
+--
+
+LOCK TABLES `employee_salary` WRITE;
+/*!40000 ALTER TABLE `employee_salary` DISABLE KEYS */;
+INSERT INTO `employee_salary` VALUES (1,'e','Knope','Deputy Director of Parks and Recreation',75000,1),(2,'Ron','Swanson','Director of Parks and Recreation',70000,1),(3,'Tom','Haverford','Entrepreneur',50000,1),(4,'April','Ludgate','Assistant to the Director of Parks and Recreation',25000,1),(5,'Jerry','Gergich','Office Manager',50000,1),(6,'Donna','Meagle','Office Manager',60000,1),(7,'Ann','Perkins','Nurse',55000,4),(8,'Chris','Traeger','City Manager',90000,3),(9,'Ben','Wyatt','State Auditor',70000,6),(10,'Andy','Dwyer','Shoe Shiner and Musician',20000,2),(11,'Mark','Brendanawicz','City Planner',57000,3),(12,'Craig','Middlebrooks','Parks Director',65000,1),(13,'NEW','TEST','CEO',1000000,NULL);
+/*!40000 ALTER TABLE `employee_salary` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `employee_insert` AFTER INSERT ON `employee_salary` FOR EACH ROW BEGIN
+	INSERT INTO employee_demographics (employee_id, first_name, last_name) 
+	VALUES (NEW.employee_id, NEW.first_name, NEW.last_name);
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-20 23:44:10
